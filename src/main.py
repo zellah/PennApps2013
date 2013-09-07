f38cc876c42fde5c9fe6c4c571d1f77c339b482e
Zella Henderson <zhenders@smith.edu>
2013-09-07 02:23:05 -0700

    Renders homepage from templates

--- src/main.py
+++ src/main.py
@@ -86,7 +86,7 @@ def get_user_data(userid):
 @app.route('/')
 @login_required
 def home():
-    return "Hello World!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
